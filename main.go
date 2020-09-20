296363880211bd8b0ad378ead885b297d9edc46c
Theodor Berkinblit <fb777@ya.ru>
2020-09-20 13:49:19 +0300

    applied go fmt

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 func main() {
-	var balance int64 = 15000000_00       // 15 миллионов в копейках
-	var transfer int64 = 10000000_00     // 10 миллионов в копейках
-	total := balance + transfer 
+	var balance int64 = 15000000_00  // 15 миллионов в копейках
+	var transfer int64 = 10000000_00 // 10 миллионов в копейках
+	total := balance + transfer
 	println(total)
 }
-
